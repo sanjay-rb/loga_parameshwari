e138cb2530c30d9aa08c97b14f3a522d103ae075
sanjaybabu618 <sanjaybabu618@gmail.com>
2021-03-10 10:35:45 +0530

    Image Picker Added

--- lib/detail_pooja_screen/detail_pooja_screen.dart
+++ lib/detail_pooja_screen/detail_pooja_screen.dart
@@ -3,14 +3,19 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:intl/intl.dart';
 import 'package:loga_parameshwari/constant/constant.dart';
 import 'package:loga_parameshwari/model/pooja.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
-class DetailPooja extends StatelessWidget {
+class DetailPooja extends StatefulWidget {
   //const
   DetailPooja({Key key, this.pooja}) : super(key: key);
   final Pooja pooja;
 
+  @override
+  _DetailPoojaState createState() => _DetailPoojaState();
+}
+
+class _DetailPoojaState extends State<DetailPooja> {
   final List<String> images = [
-    "https://uae.microless.com/cdn/no_image.jpg",
     "https://images-na.ssl-images-amazon.com/images/I/81aF3Ob-2KL._UX679_.jpg",
     "https://www.boostmobile.com/content/dam/boostmobile/en/products/phones/apple/iphone-7/silver/device-front.png.transform/pdpCarousel/image.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUgs8_kmuhScsx-J01d8fA1mhlCR5-1jyvMYxqCB8h3LCqcgl9Q",
@@ -22,73 +27,70 @@ class DetailPooja extends StatelessWidget {
     "https://media.onthemarket.com/properties/6191840/797152761/composite.jpg",
   ];
 
+  List<Asset> upImages = <Asset>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          upImages = await MultiImagePicker.pickImages(
+            maxImages: 500,
+            enableCamera: true,
+            materialOptions: MaterialOptions(
+              actionBarTitle: "Upload Images",
+              allViewTitle: "All Photos",
+              useDetailsView: false,
+            ),
+          );
+          setState(() {
+            upImages = upImages;
+          });
+          print("HI THIS IS RESULE $upImages");
+        },
+        child: Icon(Icons.add),
+      ),
       body: SafeArea(
         child: Column(
           children: [
             Text(
-              "${pooja.name}",
+              "${widget.pooja.name}",
               style: TextDesign.headText,
             ),
             Text(
-              "by ${pooja.by}",
+              "by ${widget.pooja.by}",
               style: TextDesign.titleText,
             ),
             Text(
-              "${DateFormat("dd-MM-yyyy (hh:mm aaa)").format(pooja.on.toDate())}",
+              "${DateFormat("dd-MM-yyyy (hh:mm aaa)").format(widget.pooja.on.toDate())}",
               style: TextDesign.subTitleText,
             ),
             Divider(),
-            Expanded(
-              child: StaggeredGridView.countBuilder(
-                crossAxisCount: 4,
-                itemCount: images.length,
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return InkWell(
-                      onTap: () {
-                        print("HI");
+            (upImages.length != 0)
+                ? Expanded(
+                    child: StaggeredGridView.countBuilder(
+                      crossAxisCount: 4,
+                      itemCount: upImages.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          elevation: 10,
+                          child: AssetThumb(
+                            asset: upImages[index],
+                            width: 500,
+                            height: 500,
+                          ),
+                        );
                       },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: 150,
-                          height: 150,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border.all(),
-                            borderRadius: CardContainer.borderRadius,
-                          ),
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: Icon(Icons.add, size: 50),
-                                  ),
-                                  Text("Add Images")
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  } else {
-                    return Card(
-                      elevation: 10,
-                      child: Image.network(images[index]),
-                    );
-                  }
-                },
-                staggeredTileBuilder: (int index) => new StaggeredTile.fit(2),
-                mainAxisSpacing: 4.0,
-                crossAxisSpacing: 4.0,
-              ),
-            ),
+                      staggeredTileBuilder: (int index) =>
+                          new StaggeredTile.fit(2),
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                    ),
+                  )
+                : Expanded(
+                    child: Center(
+                    child: Text('Add Images by clicking below "+" button'),
+                  )),
           ],
         ),
       ),
