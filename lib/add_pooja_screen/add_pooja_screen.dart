8303b62b0da81a08370aa75a56525ba6575ef710
sanjaybabu618 <sanjaybabu618@gmail.com>
2021-03-14 15:34:07 +0530

    Added Notification

--- lib/add_pooja_screen/add_pooja_screen.dart
+++ lib/add_pooja_screen/add_pooja_screen.dart
@@ -144,6 +144,42 @@ class _AddPoojaScreenState extends State<AddPoojaScreen> {
                                       .toJson(),
                                 )
                                 .then((value) {
+                              /**
+                                   Future<void> sendPushMessage() async {
+    if (_token == null) {
+      print('Unable to send FCM message, no token exists.');
+      return;
+    }
+
+    String constructFCMPayload(String token) {
+  _messageCount++;
+  return jsonEncode({
+    'token': token,
+    'data': {
+      'via': 'FlutterFire Cloud Messaging!!!',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello FlutterFire!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
+}
+
+    try {
+      await http.post(
+        Uri.parse('https://api.rnfirebase.io/messaging/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: constructFCMPayload(_token),
+      );
+      print('FCM request for device sent!');
+    } catch (e) {
+      print(e);
+    }
+  }
+                                   */
                               showDialog(
                                 context: context,
                                 builder: (context) => AlertDialog(
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loga_parameshwari/home_screen/home_screen.dart';
@@ -10,6 +11,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   // Admob.initialize();
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  await FirebaseMessaging.instance.subscribeToTopic("all");
   runApp(MyApp());
 }
 
